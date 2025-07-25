f69b5970eb39b376962a506783763e5f4cdb5ddb
chloe-hsieh <chloehsieh@jetfuel.com.tw>
2025-07-25 15:58:59 +0800

    add type to .env

--- src/components/layouts/headers/Header.tsx
+++ src/components/layouts/headers/Header.tsx
@@ -43,7 +43,7 @@ const Header: React.FC = () => {
 
         <LogoContainer>
           <FaGlobe size={28} style={{ marginRight: '8px' }} />
-          <WebsiteName>Globetrekker</WebsiteName>
+          <WebsiteName>GlobeTrekker</WebsiteName>
         </LogoContainer>
       
         {/* 桌面版 Nav 與 Search */}
