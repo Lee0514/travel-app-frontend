dcbb32be1542572b25a024e06c9026c502ede995
chloe-hsieh <100898369+Chloe905@users.noreply.github.com>
2026-03-03 15:59:51 +0800

    remove initail collection

--- src/types/collection.tsx
+++ src/types/collection.tsx
@@ -15,10 +15,10 @@ export interface Collection {
  * 如果之後要改預設分類只要改這裡
  */
 export const DEFAULT_COLLECTIONS: Collection[] = [
-  { id: 'uncategorized', name: '未分類', items: [] },
-  { id: 'switzerland', name: '瑞士', items: [] },
-  { id: 'paris', name: '巴黎', items: [] },
-  { id: 'tokyo', name: '東京', items: [] }
+  { id: 'uncategorized', name: '未分類', items: [] }
+  // { id: 'switzerland', name: '瑞士', items: [] },
+  // { id: 'paris', name: '巴黎', items: [] },
+  // { id: 'tokyo', name: '東京', items: [] }
 ];
 
 export interface CollectionsState {
